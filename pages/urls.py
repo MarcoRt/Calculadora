b35b952c34f59ef99acb2159fce6842b7e7beaed
MarcoRt <nomarcort@gmail.com>
2021-09-25 13:19:52 -0500

    first commit

---
+++ pages/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from .views import (HomePageView,
+    FAQPageView,
+    AboutPageView,
+    ContactPageView,
+    TeoriaPageView,
+    SeleccionPageView,
+    ProyeccionPageView,
+    ConjuntosPageView,
+    ProductoCartesianoPageView,
+    ReunionPageView,
+    PracticaPageView
+    )
+urlpatterns = [
+    path('faq/',FAQPageView.as_view(),name='faq'),
+    path('',HomePageView.as_view(),name='index'),
+    path('index/',HomePageView.as_view(),name='index'),
+    path('about/',AboutPageView.as_view(),name='about'),
+    path('contact/',ContactPageView.as_view(),name='contact'),
+    path('teoria/',TeoriaPageView.as_view(),name='teoria'),
+    path('teoria/seleccion/',SeleccionPageView.as_view(),name='seleccion'),
+    path('teoria/proyeccion/',ProyeccionPageView.as_view(),name='proyeccion'),
+    path('teoria/conjuntos/',ConjuntosPageView.as_view(),name='conjuntos'),
+    path('teoria/productoCartesiano/',ProductoCartesianoPageView.as_view(),name='productoCartesiano'),
+    path('teoria/reunion/',ReunionPageView.as_view(),name='reunion'),
+    path('practica/',PracticaPageView.as_view(),name='practica'),
+]
---
+++ pages/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'index.html'
+
+class FAQPageView(TemplateView):
+    template_name = "faq.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+class ContactPageView(TemplateView):
+    template_name = "contact.html"
+
+class TeoriaPageView(TemplateView):
+    template_name = "teoria.html"
+
+class SeleccionPageView(TemplateView):
+    template_name = "seleccion.html"
+
+class ProyeccionPageView(TemplateView):
+    template_name = "proyeccion.html"
+
+class ConjuntosPageView(TemplateView):
+    template_name = "conjuntos.html"
+
+class ProductoCartesianoPageView(TemplateView):
+    template_name = "productoCartesiano.html"
+
+class ReunionPageView(TemplateView):
+    template_name = "reunion.html"
+
+class PracticaPageView(TemplateView):
+    template_name = "practica.html"

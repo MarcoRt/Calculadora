251bb51c53047cd4de22f601a2ec4f5d857a1c8d
Humberto Vazquez <humbertoipnvz@gmail.com>
2021-10-01 16:40:04 -0500

    Nueva interfaz

--- pages/views.py
+++ pages/views.py
@@ -8,28 +8,28 @@ class FAQPageView(TemplateView):
     template_name = "faq.html"
 
 class AboutPageView(TemplateView):
-    template_name = "about.html"
+    template_name = "acerca.html"
 
 class ContactPageView(TemplateView):
-    template_name = "contact.html"
+    template_name = "contacto.html"
 
 class TeoriaPageView(TemplateView):
     template_name = "teoria.html"
 
 class SeleccionPageView(TemplateView):
-    template_name = "seleccion.html"
+    template_name = "teoria-seleccion.html"
 
 class ProyeccionPageView(TemplateView):
-    template_name = "proyeccion.html"
+    template_name = "teoria-proyeccion.html"
 
 class ConjuntosPageView(TemplateView):
-    template_name = "conjuntos.html"
+    template_name = "teoria-conjuntos.html"
 
 class ProductoCartesianoPageView(TemplateView):
-    template_name = "productoCartesiano.html"
+    template_name = "teoria-producto.html"
 
 class ReunionPageView(TemplateView):
-    template_name = "reunion.html"
+    template_name = "teoria-reunion.html"
 
 class PracticaPageView(TemplateView):
     template_name = "practica.html"

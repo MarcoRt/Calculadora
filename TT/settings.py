0f3dccea7c2878a957b958b7cbc2e684936db6b4
Humberto <humbertoipnvz@gmail.com>
2021-11-09 11:28:31 -0600

    comentada la BBDD en settings

--- TT/settings.py
+++ TT/settings.py
@@ -74,16 +74,16 @@ WSGI_APPLICATION = 'TT.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'escuela',
-        'USER': 'marcort',
-        'PASSWORD': '%Znbf7hUYj6u',
-        'HOST': 'localhost',
-        'PORT': '3306'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'escuela',
+#         'USER': 'marcort',
+#         'PASSWORD': '%Znbf7hUYj6u',
+#         'HOST': 'localhost',
+#         'PORT': '3306'
+#     }
+# }
 
 
 # Password validation
